f2bef72940c7aca7a7f06a64644b434122b40f53
naitikdbansal <labsconnectbusiness@gmail.com>
2025-05-24 14:09:30 +0530

    Add files via upload

---
+++ AreaSquare.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int side;
+int area;
+cout<<"enter side of square (in meters)"<<endl;
+cin>>side;
+area = side*side;
+cout<<"Area of given square is: "<<area<<" sq.metres"<<endl;
+return 0;
+}
